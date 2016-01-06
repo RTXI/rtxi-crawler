74f7fda29ffc1cc5995692e58749579f0118d733
anselg <aesthapus@gmail.com>
2016-01-05 19:41:19 -0500

    write crawler to call itself recursively...

--- crawler.sh
+++ crawler.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-curl -sA "Chrome" -k -L https://scholar.google.com/scholar?q=rtxi -o page.html
+URL="https://scholar.google.com/scholar?q=rtxi"
+
+#while $ERROR; do
+#	curl -sA "Chrome" -k -L $URL | ./crawler.py
+#done
+
+curl -sA "Chrome" -k -L $URL | ./crawler.py

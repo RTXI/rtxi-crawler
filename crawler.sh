87addcffa8e46f3ea6cb02f4179518d931ac1eba
anselg <aesthapus@gmail.com>
2016-01-06 06:11:37 -0500

    using pycurl, so bash script with curl isn't needed

--- crawler.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-URL="https://scholar.google.com/scholar?q=rtxi"
-
-#while $ERROR; do
-#	curl -sA "Chrome" -k -L $URL | ./crawler.py
-#done
-
-curl -sA "Chrome" -k -L $URL | ./crawler.py
--- scraper.py
+++ scraper.py
@@ -1,3 +1,32 @@
 #! /bin/env python
 
-from bs4 import BeaututifulSoup
+from bs4 import BeautifulSoup
+from io import BytesIO
+import pycurl
+import sqlite3 as sql
+import sys
+import re
+
+################################################################################
+# Global variables
+################################################################################
+url = "https://scholar.google.com/scholar?q=rtxi"
+terms = [ 'rtxi', 'RTXI', 'real-time experiment interface']
+
+################################################################################
+# Initialize buffers and download initial page
+################################################################################
+buffer = BytesIO()
+curl = pycurl.Curl()
+curl.setopt(pycurl.URL, url)
+curl.setopt(pycurl.SSL_VERIFYPEER, 1)
+curl.setopt(pycurl.SSL_VERIFYHOST, 2)
+curl.setopt(curl.WRITEDATA, buffer)
+
+# download the page and write bytecode to buffer
+curl.perform()
+curl.close()
+html = buffer.getvalue().decode('iso-8859-1')
+
+# set up parser
+soup = BeautifulSoup(html, 'html.parser')
